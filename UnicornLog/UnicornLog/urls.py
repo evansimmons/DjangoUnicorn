bfa3bbcfebbfc315be8742df56a611e4c5949593
etscodelancer <etscodelancer@gmail.com>
2021-07-08 10:05:36 -0700

    Lots of changes

--- UnicornLog/UnicornLog/urls.py
+++ UnicornLog/UnicornLog/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+#from UnicornLog.UnicornLog_REST import views
+
+router = routers.DefaultRouter()
+#router.register(r'users', views.UserViewSet)
+#router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
--- UnicornLog/UnicornLog_REST/models.py
+++ UnicornLog/UnicornLog_REST/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import ForeignKey
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+#from django_countries.fields import CountryField
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+
 
 # Create your models here.
+
+class Location(models.Model):
+    """the location of a sighting"""
+    name = models.CharField(max_length=255)
+#    country = CountryField()
+    def __str__(self):
+        """returns thy self as text"""
+        return self.text    
+
+class Unicorn(models.Model):
+    """this is a Unicorn"""
+    name = models.CharField(max_length=255)
+    class Color(models.TextChoices):
+        white = "White"
+        gray = "Gray"
+        black = "black"
+        Sparkle= "Sparkle"
+        rainbow = "Rainbow"
+    color = Color.choices
+    RainbowPoop = models.BooleanField(default=False)
+    description = models.TextField()
+    def __str__(self):
+        """returns thy self as text"""
+        return self.text
+
+class Sighting(models.Model):
+    """an individual sighting of a unicorn"""
+    location = models.ForeignKey(Location, on_delete=CASCADE)
+    description = models.TextField()
+    unicorn = models.ForeignKey(Unicorn, on_delete=CASCADE)
+    date = models.DateField(("%m/%d/%Y"), auto_now=False, auto_now_add=False)
+    def __str__(self):
+        """returns thy self as text"""
+        return self.text
